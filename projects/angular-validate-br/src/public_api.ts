5b4ef1624531a6cb656b57558e57ff7873e2f338
marcus.santana <marcus.santana@paggcerto.com.br>
2018-07-05 12:09:00 -0300

    Remove import

--- projects/angular-validate-br/src/public_api.ts
+++ projects/angular-validate-br/src/public_api.ts
@@ -3,5 +3,4 @@
  */
 
 export * from './lib/modules/angular-validate-br.module';
-export { PropNameValidatorDirective as PropName } from './lib/directives/propName-validator-directive';
 export { ValidateBr } from './lib/class/validate-br';
