ebfe083ff49d570f904186b4ac96a3fdd0cd7081
marcus.santana <marcus.santana@paggcerto.com.br>
2018-07-06 10:43:34 -0300

    Themes v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ContactComponent } from './samples/components/contact/contact.component';
 import { AngularValidateBrLibModule } from 'projects/angular-validate-br/src/public_api';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [AppComponent, ContactComponent],
+  declarations: [AppComponent, ContactComponent, HeaderComponent, SidebarComponent, FooterComponent, NavBarComponent],
   imports: [BrowserModule, ReactiveFormsModule, AngularValidateBrLibModule],
   providers: [],
   bootstrap: [AppComponent]
